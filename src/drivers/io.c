920710f67461a1dda68c24caa3e666ad90300ce1
jiwonj05 <jiwonj1122@gmail.com>
2024-09-14 13:57:21 -0700

    added all

--- src/drivers/io.c
+++ src/drivers/io.c
@@ -1,10 +1,55 @@
 #include "io.h"
 #include "defines.h"
 
+/* Enums are represented as 16-bit by default on MSP43. The enum value can be viewed as:
+ * [ Zeros (11 bits) | Port (2 bits) | pin (3 bits)] */
+
+
+#define IO_PORT_OFFSET (3u) //this represents the position of where the port bits are located
+#define IO_PORT_MASK (0x3u << IO_PORT_OFFSET)
+#define IO_PIN_MASK (0x7u)
+
+static uint8_t io_port(io_e io)
+{
+    return (io & IO_PORT_MASK) >> IO_PORT_OFFSET;
+}
+
+static inline uint8_t io_pin_idx(io_e io)
+{
+    return io & IO_PIN_MASK;
+}
+
+static uint8_t io_pin_bit(io_e io)
+{
+    return 1 << io_pin_idx(io);
+}
+
+
 void io_set_select(io_e io, io_select_e select)
 {
     UNUSED(io);
     UNUSED(select);
+    switch (select)
+    {
+	    case IO_SELECT_GPIO:
+		    switch (io_port(io)){
+		    case 0:
+			    break;
+		    case 1:
+		            break;
+		    case 2:
+			    break;
+		    
+		    }
+		    break
+
+    
+    
+    
+    
+    
+    }
+   
     // TODO: implementation
 }
 

8f77a01027f690a7b85b003b17c871f59fe745c5
jiwonj05 <jiwonj1122@gmail.com>
2024-10-06 03:53:49 -0400

    feat(io): handle IO interrupts
    Add functions for configuring IO interrupts. Make it possible to pass an interrupt sercive routine
    (ISR) as an argument, and have that be called inside the corresponding port interrupt function.
    Access the interrupt related registers in the same way as the rest of the IO registers.

--- tools/build_tests.sh
+++ tools/build_tests.sh
@@ -6,7 +6,7 @@ TEST_FUNCTIONS=$(cat src/test/test.c | grep -P '(?<=void )test_.+(?=\()' -o)
 for test_function in $TEST_FUNCTIONS
 do
     make HW=LAUNCHPAD TEST=$test_function
-    make HW=NSUMO TEST=$test_function
-    cd /home/jiwon/sumobot/tools
+    make HW=SBOT TEST=$test_function
+    cd /home/jiwon/sumobot
     ./cleanup.sh
 done
